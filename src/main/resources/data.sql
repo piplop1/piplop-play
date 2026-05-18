276d5c9102b7e619bbf383195b3f0bd62d741b55
piplop1 <leonardo.cuevadiaz@gmail.com>
2026-05-18 18:05:30 -0500

    feat: añadir 40 peliculas a data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -69,3 +69,163 @@ VALUES ('Gladiador', 155, 'ACTION', 4.5, '2000-05-05', 'AVAILABLE')
 INSERT INTO piplop_play_peliculas (titulo, duracion, genero, clasificacion, fecha_estreno, estado)
 VALUES ('El Resplandor', 146, 'HORROR', 4.4, '1980-05-23', 'AVAILABLE')
     ON CONFLICT (titulo) DO NOTHING;
+
+INSERT INTO piplop_play_peliculas (titulo, duracion, genero, clasificacion, fecha_estreno, estado)
+VALUES ('The Godfather Part II', 202, 'DRAMA', 4.9, '1974-12-20', 'AVAILABLE')
+    ON CONFLICT (titulo) DO NOTHING;
+
+INSERT INTO piplop_play_peliculas (titulo, duracion, genero, clasificacion, fecha_estreno, estado)
+VALUES ('Pulp Fiction', 154, 'DRAMA', 4.8, '1994-10-14', 'AVAILABLE')
+    ON CONFLICT (titulo) DO NOTHING;
+
+INSERT INTO piplop_play_peliculas (titulo, duracion, genero, clasificacion, fecha_estreno, estado)
+VALUES ('The Shawshank Redemption', 142, 'DRAMA', 5.0, '1994-09-23', 'AVAILABLE')
+    ON CONFLICT (titulo) DO NOTHING;
+
+INSERT INTO piplop_play_peliculas (titulo, duracion, genero, clasificacion, fecha_estreno, estado)
+VALUES ('Forrest Gump', 142, 'DRAMA', 4.8, '1994-07-06', 'AVAILABLE')
+    ON CONFLICT (titulo) DO NOTHING;
+
+INSERT INTO piplop_play_peliculas (titulo, duracion, genero, clasificacion, fecha_estreno, estado)
+VALUES ('The Lion King', 88, 'ANIMATED', 4.7, '1994-06-24', 'AVAILABLE')
+    ON CONFLICT (titulo) DO NOTHING;
+
+INSERT INTO piplop_play_peliculas (titulo, duracion, genero, clasificacion, fecha_estreno, estado)
+VALUES ('Goodfellas', 146, 'DRAMA', 4.8, '1990-09-19', 'AVAILABLE')
+    ON CONFLICT (titulo) DO NOTHING;
+
+INSERT INTO piplop_play_peliculas (titulo, duracion, genero, clasificacion, fecha_estreno, estado)
+VALUES ('Schindler''s List', 195, 'DRAMA', 5.0, '1993-12-15', 'AVAILABLE')
+    ON CONFLICT (titulo) DO NOTHING;
+
+INSERT INTO piplop_play_peliculas (titulo, duracion, genero, clasificacion, fecha_estreno, estado)
+VALUES ('The Silence of the Lambs', 118, 'HORROR', 4.7, '1991-02-14', 'AVAILABLE')
+    ON CONFLICT (titulo) DO NOTHING;
+
+INSERT INTO piplop_play_peliculas (titulo, duracion, genero, clasificacion, fecha_estreno, estado)
+VALUES ('Jurassic Park', 127, 'SCI_FI', 4.5, '1993-06-11', 'AVAILABLE')
+    ON CONFLICT (titulo) DO NOTHING;
+
+INSERT INTO piplop_play_peliculas (titulo, duracion, genero, clasificacion, fecha_estreno, estado)
+VALUES ('Back to the Future', 116, 'SCI_FI', 4.8, '1985-07-03', 'AVAILABLE')
+    ON CONFLICT (titulo) DO NOTHING;
+
+INSERT INTO piplop_play_peliculas (titulo, duracion, genero, clasificacion, fecha_estreno, estado)
+VALUES ('Spirited Away', 125, 'ANIMATED', 4.9, '2001-07-20', 'AVAILABLE')
+    ON CONFLICT (titulo) DO NOTHING;
+
+INSERT INTO piplop_play_peliculas (titulo, duracion, genero, clasificacion, fecha_estreno, estado)
+VALUES ('Your Name', 112, 'ANIMATED', 4.8, '2016-08-26', 'AVAILABLE')
+    ON CONFLICT (titulo) DO NOTHING;
+
+INSERT INTO piplop_play_peliculas (titulo, duracion, genero, clasificacion, fecha_estreno, estado)
+VALUES ('Parasite', 132, 'DRAMA', 4.9, '2019-05-30', 'AVAILABLE')
+    ON CONFLICT (titulo) DO NOTHING;
+
+INSERT INTO piplop_play_peliculas (titulo, duracion, genero, clasificacion, fecha_estreno, estado)
+VALUES ('Joker', 122, 'DRAMA', 4.5, '2019-10-04', 'AVAILABLE')
+    ON CONFLICT (titulo) DO NOTHING;
+
+INSERT INTO piplop_play_peliculas (titulo, duracion, genero, clasificacion, fecha_estreno, estado)
+VALUES ('1917', 119, 'DRAMA', 4.6, '2019-12-25', 'AVAILABLE')
+    ON CONFLICT (titulo) DO NOTHING;
+
+INSERT INTO piplop_play_peliculas (titulo, duracion, genero, clasificacion, fecha_estreno, estado)
+VALUES ('Spider-Man: Into the Spider-Verse', 117, 'ANIMATED', 4.8, '2018-12-14', 'AVAILABLE')
+    ON CONFLICT (titulo) DO NOTHING;
+
+INSERT INTO piplop_play_peliculas (titulo, duracion, genero, clasificacion, fecha_estreno, estado)
+VALUES ('Mad Max: Fury Road', 120, 'ACTION', 4.7, '2015-05-15', 'AVAILABLE')
+    ON CONFLICT (titulo) DO NOTHING;
+
+INSERT INTO piplop_play_peliculas (titulo, duracion, genero, clasificacion, fecha_estreno, estado)
+VALUES ('The Wolf of Wall Street', 180, 'COMEDY', 4.5, '2013-12-25', 'AVAILABLE')
+    ON CONFLICT (titulo) DO NOTHING;
+
+INSERT INTO piplop_play_peliculas (titulo, duracion, genero, clasificacion, fecha_estreno, estado)
+VALUES ('Django Unchained', 165, 'ACTION', 4.7, '2012-12-25', 'AVAILABLE')
+    ON CONFLICT (titulo) DO NOTHING;
+
+INSERT INTO piplop_play_peliculas (titulo, duracion, genero, clasificacion, fecha_estreno, estado)
+VALUES ('Inglourious Basterds', 153, 'ACTION', 4.6, '2009-08-21', 'AVAILABLE')
+    ON CONFLICT (titulo) DO NOTHING;
+
+INSERT INTO piplop_play_peliculas (titulo, duracion, genero, clasificacion, fecha_estreno, estado)
+VALUES ('The Grand Budapest Hotel', 99, 'COMEDY', 4.5, '2014-03-28', 'AVAILABLE')
+    ON CONFLICT (titulo) DO NOTHING;
+
+INSERT INTO piplop_play_peliculas (titulo, duracion, genero, clasificacion, fecha_estreno, estado)
+VALUES ('Whiplash', 107, 'DRAMA', 4.8, '2014-10-10', 'AVAILABLE')
+    ON CONFLICT (titulo) DO NOTHING;
+
+INSERT INTO piplop_play_peliculas (titulo, duracion, genero, clasificacion, fecha_estreno, estado)
+VALUES ('La La Land', 128, 'COMEDY', 4.5, '2016-12-09', 'AVAILABLE')
+    ON CONFLICT (titulo) DO NOTHING;
+
+INSERT INTO piplop_play_peliculas (titulo, duracion, genero, clasificacion, fecha_estreno, estado)
+VALUES ('Get Out', 104, 'HORROR', 4.5, '2017-02-24', 'AVAILABLE')
+    ON CONFLICT (titulo) DO NOTHING;
+
+INSERT INTO piplop_play_peliculas (titulo, duracion, genero, clasificacion, fecha_estreno, estado)
+VALUES ('A Quiet Place', 90, 'HORROR', 4.4, '2018-04-06', 'AVAILABLE')
+    ON CONFLICT (titulo) DO NOTHING;
+
+INSERT INTO piplop_play_peliculas (titulo, duracion, genero, clasificacion, fecha_estreno, estado)
+VALUES ('Hereditary', 127, 'HORROR', 4.2, '2018-06-08', 'AVAILABLE')
+    ON CONFLICT (titulo) DO NOTHING;
+
+INSERT INTO piplop_play_peliculas (titulo, duracion, genero, clasificacion, fecha_estreno, estado)
+VALUES ('Midsommar', 148, 'HORROR', 4.0, '2019-07-03', 'AVAILABLE')
+    ON CONFLICT (titulo) DO NOTHING;
+
+INSERT INTO piplop_play_peliculas (titulo, duracion, genero, clasificacion, fecha_estreno, estado)
+VALUES ('Knives Out', 130, 'COMEDY', 4.6, '2019-11-27', 'AVAILABLE')
+    ON CONFLICT (titulo) DO NOTHING;
+
+INSERT INTO piplop_play_peliculas (titulo, duracion, genero, clasificacion, fecha_estreno, estado)
+VALUES ('The Truman Show', 103, 'DRAMA', 4.7, '1998-06-05', 'AVAILABLE')
+    ON CONFLICT (titulo) DO NOTHING;
+
+INSERT INTO piplop_play_peliculas (titulo, duracion, genero, clasificacion, fecha_estreno, estado)
+VALUES ('Eternal Sunshine of the Spotless Mind', 108, 'DRAMA', 4.6, '2004-03-19', 'AVAILABLE')
+    ON CONFLICT (titulo) DO NOTHING;
+
+INSERT INTO piplop_play_peliculas (titulo, duracion, genero, clasificacion, fecha_estreno, estado)
+VALUES ('2001: A Space Odyssey', 149, 'SCI_FI', 4.5, '1968-04-02', 'AVAILABLE')
+    ON CONFLICT (titulo) DO NOTHING;
+
+INSERT INTO piplop_play_peliculas (titulo, duracion, genero, clasificacion, fecha_estreno, estado)
+VALUES ('Blade Runner 2049', 164, 'SCI_FI', 4.5, '2017-10-06', 'AVAILABLE')
+    ON CONFLICT (titulo) DO NOTHING;
+
+INSERT INTO piplop_play_peliculas (titulo, duracion, genero, clasificacion, fecha_estreno, estado)
+VALUES ('Arrival', 116, 'SCI_FI', 4.6, '2016-11-11', 'AVAILABLE')
+    ON CONFLICT (titulo) DO NOTHING;
+
+INSERT INTO piplop_play_peliculas (titulo, duracion, genero, clasificacion, fecha_estreno, estado)
+VALUES ('Ex Machina', 108, 'SCI_FI', 4.4, '2015-01-21', 'AVAILABLE')
+    ON CONFLICT (titulo) DO NOTHING;
+
+INSERT INTO piplop_play_peliculas (titulo, duracion, genero, clasificacion, fecha_estreno, estado)
+VALUES ('Dune', 155, 'SCI_FI', 4.6, '2021-10-22', 'AVAILABLE')
+    ON CONFLICT (titulo) DO NOTHING;
+
+INSERT INTO piplop_play_peliculas (titulo, duracion, genero, clasificacion, fecha_estreno, estado)
+VALUES ('Oppenheimer', 180, 'DRAMA', 4.8, '2023-07-21', 'AVAILABLE')
+    ON CONFLICT (titulo) DO NOTHING;
+
+INSERT INTO piplop_play_peliculas (titulo, duracion, genero, clasificacion, fecha_estreno, estado)
+VALUES ('Barbie', 114, 'COMEDY', 4.2, '2023-07-21', 'AVAILABLE')
+    ON CONFLICT (titulo) DO NOTHING;
+
+INSERT INTO piplop_play_peliculas (titulo, duracion, genero, clasificacion, fecha_estreno, estado)
+VALUES ('Everything Everywhere All at Once', 139, 'SCI_FI', 4.7, '2022-03-25', 'AVAILABLE')
+    ON CONFLICT (titulo) DO NOTHING;
+
+INSERT INTO piplop_play_peliculas (titulo, duracion, genero, clasificacion, fecha_estreno, estado)
+VALUES ('The Batman', 176, 'ACTION', 4.4, '2022-03-04', 'AVAILABLE')
+    ON CONFLICT (titulo) DO NOTHING;
+
+INSERT INTO piplop_play_peliculas (titulo, duracion, genero, clasificacion, fecha_estreno, estado)
+VALUES ('Top Gun: Maverick', 130, 'ACTION', 4.6, '2022-05-27', 'AVAILABLE')
+    ON CONFLICT (titulo) DO NOTHING;
